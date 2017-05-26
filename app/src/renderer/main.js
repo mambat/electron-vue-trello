3592dca37dccc1111732cd0d89f154be0aa3f85b
niuyang <niuyang@raiyee.com>
2017-05-26 15:25:58 +0800

    feat(drag): test dragable

--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Electron from 'vue-electron';
 import Resource from 'vue-resource';
 import Router from 'vue-router';
-import Sortable from 'vue-sortable';
+import draggable from 'vuedraggable';
 
 import App from './App';
 import routes from './routes';
@@ -11,7 +11,7 @@ import store from 'renderer/vuex/store';
 Vue.use(Electron);
 Vue.use(Resource);
 Vue.use(Router);
-Vue.use(Sortable);
+Vue.component('draggable', draggable);
 Vue.config.debug = true;
 
 const router = new Router({
--- app/src/renderer/pages/BoardPage.vue
+++ app/src/renderer/pages/BoardPage.vue
@@ -9,52 +9,54 @@
         </div>
         <div class="board-canvas">
           <div id="board" class="u-fancy-scrollbar">
-            <div class="list-wrapper">
-              <div class="list">
-                <div class="list-header u-clearfix is-menu-shown">
-                  <div class="list-header-target"></div>
-                  <h2 class="list-header-name-assist" dir="auto">Advanced</h2>
-                  <textarea class="list-header-name mod-list-name" spellcheck="false" dir="auto" maxlength="512"
-                            style="overflow: hidden; word-wrap: break-word; height: 24px;">Advanced</textarea>
-                  <p class="list-header-num-cards hide">6 cards</p>
-                  <div class="list-header-extras">
+            <draggable v-model="list" @start="drag=true" @end="drag=false">
+              <div class="list-wrapper" v-for="(item,index) in list">
+                <div class="list">
+                  <div class="list-header u-clearfix is-menu-shown">
+                    <div class="list-header-target"></div>
+                    <h2 class="list-header-name-assist" dir="auto">Advanced</h2>
+                    <textarea class="list-header-name mod-list-name" spellcheck="false" dir="auto" maxlength="512"
+                              style="overflow: hidden; word-wrap: break-word; height: 24px;">Advanced</textarea>
+                    <p class="list-header-num-cards hide">6 cards</p>
+                    <div class="list-header-extras">
                   <span class="list-header-extras-subscribe hide">
                     <span class="icon-sm icon-subscribe"></span>
                   </span>
-                    <a class="list-header-extras-menu dark-hover" href="#">
-                      <span class="icon-sm icon-overflow-menu-horizontal"></span>
-                    </a>
-                  </div>
-                </div>
-                <div class="list-cards u-fancy-scrollbar u-clearfix">
-                  <div class="list-card">
-                    <div class="list-card-cover"></div>
-                    <span class="icon-sm icon-edit list-card-operation dark-hover"></span>
-                    <div class="list-card-stickers-area hide">
-                      <div class="stickers"></div>
-                    </div>
-                    <div class="list-card-details">
-                      <div class="list-card-labels"></div>
-                      <a class="list-card-title" dir="auto" href="/c/kDjRHbGk/14-use-as-many-boards-as-you-want-we-ll-make-more">
-                        <span class="card-short-id hide">#14</span>
-                        Use as many boards as you want. We'll make more!
+                      <a class="list-header-extras-menu dark-hover" href="#">
+                        <span class="icon-sm icon-overflow-menu-horizontal"></span>
                       </a>
-                      <div class="badges">
+                    </div>
+                  </div>
+                  <div class="list-cards u-fancy-scrollbar u-clearfix">
+                    <div v-show="item === 'Bar'" class="list-card">
+                      <div class="list-card-cover"></div>
+                      <span class="icon-sm icon-edit list-card-operation dark-hover"></span>
+                      <div class="list-card-stickers-area hide">
+                        <div class="stickers"></div>
+                      </div>
+                      <div class="list-card-details">
+                        <div class="list-card-labels"></div>
+                        <a class="list-card-title" dir="auto" href="/c/kDjRHbGk/14-use-as-many-boards-as-you-want-we-ll-make-more">
+                          <span class="card-short-id hide">#14</span>
+                          Use as many boards as you want. We'll make more!
+                        </a>
+                        <div class="badges">
                       <span>
                         <div class="badge is-icon-only" title="This card has a description.">
                           <span class="badge-icon icon-sm icon-description"></span>
                         </div>
                       </span>
-                        <span><span></span></span>
+                          <span><span></span></span>
+                        </div>
+                        <div class="list-card-members"></div>
                       </div>
-                      <div class="list-card-members"></div>
+                      <p class="list-card-dropzone">Drop files to upload.</p>
                     </div>
-                    <p class="list-card-dropzone">Drop files to upload.</p>
                   </div>
+                  <a class="open-card-composer" href="#">Add a card…</a>
                 </div>
-                <a class="open-card-composer" href="#">Add a card…</a>
               </div>
-            </div>
+            </draggable>
             <div class="list-wrapper mod-add is-idle">
               <form>
                 <span class="placeholder">Add a list…</span>
@@ -79,6 +81,9 @@
     name: 'board-page',
     created: function () {
       this.toggleBodyClass();
+    },
+    data () {
+      return {list: ['Foo', 'Bar', 'Baz']};
     },
     destroyed: function () {
       this.toggleBodyClass();
